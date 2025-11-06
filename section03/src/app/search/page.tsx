88cc1439194692f8a89adef395cf1b7bbcc7f81d
LimJaeHyun <guswogus81@gmail.com>
2025-11-06 18:53:50 +0900

    앱라우터에서 페이지 라우팅 하기

---
+++ section03/src/app/search/page.tsx
@@ -0,0 +1,5 @@
+export default async function Page({ searchParams }: { searchParams: Promise<{ q: string }> }) {
+  console.log(searchParams);
+  const { q } = await searchParams;
+  return <div>Search 페이지 : {q}</div>;
+}
